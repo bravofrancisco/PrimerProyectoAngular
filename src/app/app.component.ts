d02dbaa613335930ee09e112a76fecc18c881dd1
Francisco Bravo <fcobravo.23@gmail.com>
2024-10-08 21:23:26 -0300

    PrimerProyectoEnAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = '02-bases';
+  public title: string = 'App 2024';
+  public counter: number = 10;
+
+  public incrementar(value: number): void {
+    if (this.counter + value >= 0) {
+      this.counter += value;
+    } else {
+      this.counter = 0;
+    }
+  }
+  public reset(): void {
+    this.counter = 10;
+  }
 }
