78a85393e71e67d45f5402087fb88b46b3bcd87f
Francisco Bravo <fcobravo.23@gmail.com>
2024-10-10 19:57:59 -0300

    Se agregaron carpetas interfaces,pages,services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { CounterModule } from './counter/counter.module';
-
-
-
 import { HeroesModule } from './heroes/heroes.module';
 import { ListComponent } from './heroes/list/list.component';
-
-
 
 @NgModule({
   declarations: [
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { HeroComponent } from "./hero/hero.component";
 import { CommonModule } from "@angular/common";
-// import { ListComponent } from "./list/list.component";
 
 @NgModule({
     declarations:[HeroComponent],
