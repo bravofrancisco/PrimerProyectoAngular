aa50f767adf0082cbbf1dbf7e6e38407574ff438
Francisco Bravo <fcobravo.23@gmail.com>
2024-10-09 21:32:16 -0300

    Aprendiendo Module Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
+
+import { CounterModule } from './counter/counter.module';
+
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
+  
     HeroComponent,
     ListComponent,
    
@@ -19,6 +21,7 @@ import { ListComponent } from './heroes/list/list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CounterModule
    
   ],
   providers: [],
